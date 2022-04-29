490bc65a75af762a4fce07fa3a39b3733e618809
hff2 <hophoebeho@gmail.com>
2022-04-29 14:08:41 +0800

    feat: modify the entry file

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts')
 
 var app = express();
+
+// Database setting
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/week3_Post')
+    .then(res => console.log("DB connect success"));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors())
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 module.exports = app;
