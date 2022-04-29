f9ae8e1d2e7e6a821335c4283dcf3a9ae263d450
hff2 <hophoebeho@gmail.com>
2022-04-29 14:21:49 +0800

    chore: add createError

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
+var createError = require('http-errors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts')
@@ -25,5 +26,9 @@ app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
+
+app.use(function (req, res, next) {
+    next(createError(404,"This page doesn't exit."))
+})
 
 module.exports = app;
