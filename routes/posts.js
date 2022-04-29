5f672002d2be1b0dcd3438a10edbdd91afad2fd7
hff2 <hophoebeho@gmail.com>
2022-04-29 11:29:23 +0800

    feat: add the posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const postsControllers = require('../controllers/posts')
-const httpControllers = require('../controllers/http')
 
-router.get('/', postsControllers.getPosts);
+router.get('/', postsControllers.getPosts)
 router.post('/', postsControllers.createPosts)
-router.delete('/',postsControllers.deleteAllPosts) 
 
+router.delete('/',postsControllers.deleteAllPosts) 
 router.delete('/:id',postsControllers.deleteOnePost) 
 router.patch('/:id', postsControllers.patchPosts)
 
